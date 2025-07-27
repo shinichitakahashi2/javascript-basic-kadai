3a7d20b347eba156e04becde9a1d301dedb490bb
shinichitakahashi2 <shinichi.takahashi2@gmail.com>
2025-07-27 14:36:05 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
 const timeday = new Date(Date.now());
 
 const year = timeday.getFullYear();
-const month = timeday.getMonth();
+const month = timeday.getMonth() + 1;
 const day = timeday.getDate();
 console.log(year + "年" + month + "月" + day + "日");
